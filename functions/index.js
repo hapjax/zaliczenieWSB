78f4e96e8a96a95347c084e5ae44db7991be045a
hapjax <hapjax@ymail.com>
2019-06-04 15:50:59 +0200

    final commit after hosting

--- functions/index.js
+++ functions/index.js
@@ -2,31 +2,31 @@
 const firebaseFunctions = require('firebase-functions');
 
 // The Firebase Admin SDK to access the Firebase Realtime Database.
-const admin = require('firebase-admin');
-admin.initializeApp();
+// const admin = require('firebase-admin');
+// admin.initializeApp();
 
-const nodemailer = require('nodemailer');
-const gmailEmail = encodeURIComponent(firebaseFunctions.config().gmail.email);
-const gmailPassword = encodeURIComponent(firebaseFunctions.config().gmail.password);
-const mailTransport = nodemailer.createTransport(`smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com`);
+// const nodemailer = require('nodemailer');
+// const gmailEmail = encodeURIComponent(firebaseFunctions.config().gmail.email);
+// const gmailPassword = encodeURIComponent(firebaseFunctions.config().gmail.password);
+// const mailTransport = nodemailer.createTransport(`smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com`);
 
-exports.sendContactMessage = firebaseFunctions.database.ref('/messages/{pushKey}').onWrite(event => {
-    const snapshot = event.data;
+// exports.sendContactMessage = firebaseFunctions.database.ref('/messages/{pushKey}').onWrite(event => {
+//     const snapshot = event.data;
   
-  // Only send email for new messages.
-    if (snapshot.previous.val() || !snapshot.val().name) {
-      return;
-    }
+//   // Only send email for new messages.
+//     if (snapshot.previous.val() || !snapshot.val().name) {
+//       return;
+//     }
     
-    const val = snapshot.val();
+//     const val = snapshot.val();
     
-    const mailOptions = {
-      to: 'test@example.com',
-      subject: `Information Request from ${val.name}`,
-      html: val.html
-    };
+//     const mailOptions = {
+//       to: 'test@example.com',
+//       subject: `Information Request from ${val.name}`,
+//       html: val.html
+//     };
   
-    return mailTransport.sendMail(mailOptions).then(() => {
-      return console.log('Mail sent to: test@example.com')
-    });
-  });
+//     return mailTransport.sendMail(mailOptions).then(() => {
+//       return console.log('Mail sent to: test@example.com')
+//     });
+//   });
